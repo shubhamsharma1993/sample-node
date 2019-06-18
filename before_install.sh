dcfc8c9e0f90c1c08cde6c3234307b59df71074b
shubhamsharma1993 <ssshubhamsharma4@gmail.com>
2019-06-18 15:49:36 +0530

    bjdb

--- before_install.sh
+++ before_install.sh
@@ -5,6 +5,7 @@ sudo apt-get install python-software-properties -y
 sudo apt-add-repository ppa:chris-lea/node.js -y
 sudo apt-get update
 sudo apt-get install nodejs -y
+sudo apt-get install npm
 
 # Install nodemon
 # sudo npm install nodemon -g
