7338bd896966be73c4fbd988d3619442054708c4
shubhamsharma1993 <ssshubhamsharma4@gmail.com>
2019-06-18 16:00:23 +0530

    dwds

--- before_install.sh
+++ before_install.sh
@@ -5,7 +5,7 @@ sudo apt-get install python-software-properties -y
 sudo apt-add-repository ppa:chris-lea/node.js -y
 sudo apt-get update
 sudo apt-get install nodejs -y
-sudo apt-get install npm
+sudo apt-get install npm -y
 
 # Install nodemon
 # sudo npm install nodemon -g
